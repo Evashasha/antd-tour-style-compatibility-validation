c1fe77452dc4657452d7f722b6a4d2bf625bcc19
evalli <evalli@tencent.com>
2024-08-08 17:15:34 +0800

    提交shadow-dom场景下，mask遮罩未收纳在shadowdom的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,79 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Divider, Space, Tour, TourProps } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
+import { StyleProvider } from '@ant-design/cssinjs';
 
 function App() {
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const shadowDomRef= useRef(null);
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  const steps: TourProps['steps'] = [
+    {
+      title: 'Upload File',
+      description: 'Put your files here.',
+      cover: (
+        <img
+          alt="tour.png"
+          src="https://user-images.githubusercontent.com/5378891/197385811-55df8480-7ff4-44bd-9d43-a7dade598d70.png"
+        />
+      ),
+      target: () => ref1.current,
+    },
+    {
+      title: 'Save',
+      description: 'Save your changes.',
+      target: () => ref2.current,
+    },
+    {
+      title: 'Other Actions',
+      description: 'Click to see other actions.',
+      target: () => ref3.current,
+    },
+  ];
+  const [shadowRoot, setShadowRoot] = useState(undefined);
+  useEffect(()=>{
+    if(!shadowDomRef.current){
+      return;
+    }
+    // @ts-ignore
+    const sr = shadowDomRef?.current?.attachShadow?.({ mode: 'closed' });
+    setShadowRoot(sr);
+    document.body.insertAdjacentElement('beforebegin', shadowDomRef.current);
+  }, []);
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div 
+        style={{
+          position: 'sticky'
+        }}
+        className='tour-shadow-holder' 
+        ref={shadowDomRef}
+      >
+
+      </div>
+      <Button type="primary" onClick={() => setOpen(true)}>
+        Begin Tour
+      </Button>
+      <Divider />
+      <Space>
+        <Button ref={ref1}> Upload</Button>
+        <Button ref={ref2} type="primary">
+          Save
+        </Button>
+        <Button ref={ref3} icon={<EllipsisOutlined />} />
+      </Space>
+      {shadowRoot && <StyleProvider container={shadowRoot}>
+       <Tour getPopupContainer={()=>shadowRoot} open={open} onClose={() => setOpen(false)} steps={steps} />
+      </StyleProvider>}
+    </>
   );
 }
 
